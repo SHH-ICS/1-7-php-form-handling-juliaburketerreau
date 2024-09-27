b3ac03aa80d4ab44176d7f9ce2ea640576baaa2d
juliaburketerreau <julia.burke-terreau@stu.ocsb.ca>
2024-09-27 13:43:51 +0000

    added layout

--- handler.php
+++ handler.php
@@ -1,22 +1,38 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $userName = "";
-    if ( isset( $_POST['userName'] ) ){
-      $userName = $_POST['userName'];
-    }
-    echo "<h1>Welcome ".$userName."!</h1>\n";
-    ?>
-    
-  </body>
-  
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Welcome Page</title>
+</head>
+
+<body>
+
+  <?php
+  $userName = "";
+  if (isset($_POST['userName'])) {
+    $userName = $_POST['userName'];
+  }
+  echo "<h1>Welcome " . $userName . "!</h1>\n";
+  ?>
+
+  <!-- No header, and the drawer stays open on larger screens (fixed drawer). -->
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer">
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">Title</span>
+      <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content"><!-- Your content goes here --></div>
+    </main>
+  </div>
+</body>
+
 </html>
